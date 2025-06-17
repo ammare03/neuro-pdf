ba350b574e9b939bf87169063695625990d504a0
Ammar Engineer <ammar.f.engineer@outlook.com>
2025-06-17 09:57:56 +0530

    feat: implement ContentSection and ProgressBar components for enhanced summary display

--- components/summaries/summary-viewer.tsx
+++ components/summaries/summary-viewer.tsx
@@ -1,42 +1,20 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { useState } from "react";
 import { NavigationControls } from "@/components/summaries/navigation-controls";
-import { set } from "date-fns";
+import ProgressBar from "./progress-bar";
+import { parseSection } from "@/utils/summary-helpers";
+import ContentSection from "./content-section";
 
-const parseSection = (section: string) => {
-  const [title, ...content] = section.split("\n");
-
-  const cleanTitle = title.startsWith("#")
-    ? title.substring(1).trim()
-    : title.trim();
-
-  const points: String[] = [];
-
-  let currentPoint = "";
-
-  content.forEach((line) => {
-    const trimmedLine = line.trim();
-    if (trimmedLine.startsWith("-")) {
-      if (currentPoint) points.push(currentPoint.trim());
-      currentPoint = trimmedLine;
-    } else if (!trimmedLine) {
-      if (currentPoint) points.push(currentPoint.trim());
-      currentPoint = "";
-    } else {
-      currentPoint += " " + trimmedLine;
-    }
-  });
-
-  if (currentPoint) points.push(currentPoint.trim());
-
-  return {
-    title: cleanTitle,
-    points: points.filter(
-      (point) => point && !point.startsWith("#") && !point.startsWith("[Choose")
-    ),
-  };
+const SectionTitle = ({ title }: { title: string }) => {
+  return (
+    <div className="flex flex-col gap-2 mb06 sticky top-0 pt-2 pb-4 bg-background/80 backdrop-blur-xs z-10">
+      <h2 className="text-3xl lg:text-4xl font-bold text-center flex items-center justify-center gap-2">
+        {title}
+      </h2>
+    </div>
+  );
 };
 
 export default function SummaryViewer({ summary }: { summary: string }) {
@@ -59,18 +37,16 @@ export default function SummaryViewer({ summary }: { summary: string }) {
 
   return (
     <Card className="relative px-2 h-[500px] sm:h-[600px] lg:h-[700px] w-full xl:w-[600px] overflow-hidden bg-linear-to-r from-background via-background/95 to-rose-500/5 backdrop-blur-lg shadow-2xl rounded-3xl border border-rose-500/10">
+      <ProgressBar sections={sections} currentSection={currentSection} />
       <div className="h-full overflow-y-auto scrollbar-hide pt-12 sm:pt-16 pb-20 sm:pb-24">
         <div className="px-4 sm:px-6">
-          <h2>{sections[currentSection]?.title || ""}</h2>
-          <ul>
-            {sections[currentSection]?.points.map((point, index) => (
-              <li key={index}>{point}</li>
-            ))}
-          </ul>
+          <SectionTitle title={sections[currentSection]?.title || ""} />
+          <ContentSection
+            title={sections[currentSection]?.title || ""}
+            points={sections[currentSection]?.points || []}
+          />
         </div>
       </div>
-
-      {JSON.stringify(sections[currentSection].points)}
 
       <NavigationControls
         currentSection={currentSection}
